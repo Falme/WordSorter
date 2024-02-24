3d491c6aa5f0fbd2dd70ae4ceb83643517a892f9
Falme Streamless <falme_streamless@hotmail.com>
2024-02-24 19:01:07 -0300

    Shelf is now Selectable from the click

--- Assets/Scripts/Shelf/Shelf.cs
+++ Assets/Scripts/Shelf/Shelf.cs
@@ -1,45 +1,70 @@
+using System;
+using System.Collections;
 using System.Text;
 using UnityEngine;
 
 public class Shelf : MonoBehaviour
 {
     [SerializeField] private Block blockPrefab;
+    [SerializeField] private Transform shelfBlocks;
+    
     private Block[] blocks;
+    private int blockPositionIndex = 0;
 
     private bool Selected {get; set;}
 
+    public Vector3 PositionOfNextLetter =>
+        new Vector3(0.8f * blockPositionIndex, 0f);
+
+    public bool IsFilled => blocks[blocks.Length - 1] != null;
+    
     private void Start()
     {
-        GenerateBlocks("CASA");
+        StartCoroutine(GenerateBlocks("CASA", 4));
     }
 
-    public void GenerateBlocks(string word)
+    public IEnumerator GenerateBlocks(string word, int shelfSize)
     {
+        if(IsWordBiggerThanShelf(word, shelfSize)) yield break;
+
         word = word.ToUpper();
 
-        blocks = new Block[word.Length];
+        blocks = new Block[shelfSize];
 
-        for (int i = 0; i < blocks.Length; i++)
+        foreach (char letter in word)
         {
-            blocks[i] = Instantiate(blockPrefab.gameObject).GetComponent<Block>();
-            blocks[i].Letter = word[i];
+            var _block = Instantiate(blockPrefab.gameObject, shelfBlocks).GetComponent<Block>();
+            _block.Letter = letter;
+
+            AppendBlock(_block);
+
+            yield return null;
         }
 
     }
 
-    public Vector3 PositionOfNextLetter()
+    private bool IsWordBiggerThanShelf(string word, int shelfSize)
     {
-        return Vector3.zero;
-    }
-
-    public bool IsFilled()
-    {
+        if (word.Length > shelfSize)
+        {
+            Debug.LogError("Number of Letters in a Shelf was Exceeded at GenerateBlocks");
+            Debug.LogError("Make Sure to not put a longer word to a smaller shelf");
+            return true;
+        }
         return false;
     }
 
     public void SelectShelf()
     {
+        Selected = !Selected;
+        blocks[blockPositionIndex-1].Highlight(Selected);
+    }
 
+    private void AppendBlock(Block block)
+    {
+        block.transform.localPosition = PositionOfNextLetter;
+        blocks[blockPositionIndex] = block;
+        blockPositionIndex++;
     }
 
     public string Stringify()
@@ -56,4 +81,6 @@ public class Shelf : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Alpha1)) Debug.Log(Stringify());
     }
+
+    public void OnMouseDown() => SelectShelf();
 }
