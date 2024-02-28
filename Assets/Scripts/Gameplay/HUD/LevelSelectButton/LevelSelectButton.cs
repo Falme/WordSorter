5fb0c9f7e040b03fced3e78036070e91574820ae
Falme Streamless <falme_streamless@hotmail.com>
2024-02-28 01:53:33 -0300

    Formatted LevelSelectButton class

--- Assets/Scripts/Gameplay/HUD/LevelSelectButton/LevelSelectButton.cs
+++ Assets/Scripts/Gameplay/HUD/LevelSelectButton/LevelSelectButton.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class LevelSelectButton : MonoBehaviour
 {
 	private const string SceneName = "LevelSelect";
+	private const string BodyText = "Return to Level Selection?";
 
 	public void ReturnToLevelSelection()
 	{
-		Popup.Instance.OpenPopup("Return to Level Selection?", PopupType.YES_NO, () =>
-		{
-			LevelManager.Instance.LoadScene(SceneName);
-		});
+		Popup.Instance.OpenPopup(
+			BodyText,
+			PopupType.YES_NO,
+			() => LevelManager.Instance.LoadScene(SceneName)
+		);
 	}
 }
