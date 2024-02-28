70da53460c379a0232e1a00602a8915925778175
Falme Streamless <falme_streamless@hotmail.com>
2024-02-28 02:02:08 -0300

    Formatted RestartButton class

--- Assets/Scripts/Gameplay/HUD/RestartButton/RestartButton.cs
+++ Assets/Scripts/Gameplay/HUD/RestartButton/RestartButton.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class RestartButton : MonoBehaviour
 {
+	private const string BodyText = "Restart the level from the beginning?";
 	[SerializeField] private GameManager gameManager;
 
 	public void RestartLevel()
 	{
-		Popup.Instance.OpenPopup("Restart the level from the beginning?", PopupType.YES_NO, () =>
-		{
-			gameManager.Restart();
-		});
+		Popup.Instance.OpenPopup(
+			BodyText,
+			PopupType.YES_NO,
+			gameManager.Restart
+			);
 	}
 }
